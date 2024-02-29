ecbc05496836f38ece5583297e940f7159129a69
aofei.li <aofei.li@pacteraedge.com>
2024-02-29 18:14:28 +0800

    fix: 共用请求方法添加自定义配置

--- src/service/http/index.ts
+++ src/service/http/index.ts
@@ -2,21 +2,23 @@ import service from './axios.ts';
 import { getQueryString, cleanRes } from './httpTools.ts';
 
 // 封装get,post,put等方法
-export const get = (url: string, query: object | undefined) => {
+export const get = (url: string, query: object | undefined, other = {}) => {
   return cleanRes(
     service({
       url: url + getQueryString(query),
-      method: 'get'
+      method: 'get',
+      ...other
     })
   );
 };
 
-export const post = (url: string, data: object) => {
+export const post = (url: string, data: object, other = {}) => {
   return cleanRes(
     service({
       url: url,
       method: 'post',
-      data
+      data,
+      ...other
     })
   );
 };
