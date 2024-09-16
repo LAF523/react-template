99ec16c66295ed0ca4fa035e06eab0d131eac2c3
aofei.li <1994823855@qq.com>
2024-09-16 22:36:27 +0800

    fix: 调整eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -42,13 +42,13 @@ module.exports = {
     'prettier/prettier': 'error',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
-    'no-unused-vars': 'error',
     'no-debugger': 2,
     'no-alert': 2,
     'no-dupe-keys': 2,
     'no-dupe-args': 2,
     'no-use-before-define': [2, { functions: false }],
     '@typescript-eslint/no-explicit-any': ['off'],
-    'react/prop-types': 'off' // 使用ts的参数类型检查
+    'react/prop-types': 'off', // 使用ts的参数类型检查
+    'no-unused-vars': ['error', { varsIgnorePattern: '^_' }]
   }
 };
